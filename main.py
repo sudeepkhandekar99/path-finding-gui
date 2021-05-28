c79a9f0bb138c3ad51beef5276ce519f44ccef81
sudeepkhandekar99 <sudeepkhandekar99@gmail.com>
2021-05-28 11:28:42 +0530

    drawing the grid

--- main.py
+++ main.py
@@ -104,3 +104,21 @@ def make_grid(rows, width):
 
     return grid 
 
+def draw_grid(win, rows, width):
+    gap = width // rows
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+    for j in range(rows):
+        pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))   
+    
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+
+    draw_grid(win, rows, width)
+    pygame.display.update()
+
+# def get_clicked_pos(pos, rows, width):
